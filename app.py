f3c1144647f264ed5d17846f1adc7ae65f386385
GustavoGuerato <guerato.gustavo14@gmail.com>
2023-12-27 17:44:29 -0300

    bug fixes

--- app.py
+++ app.py
@@ -9,28 +9,20 @@ class Cadastro(QMainWindow, Ui_CadastroWindow):
         super().__init__(parent)
         self.setupUi(self)
 
-        self.SignUpButton.clicked.connect(self.abrir_login)
-
-    def abrir_login(self):
-        login.show()
-        self.close()
-
 
 class Login(QMainWindow, Ui_Login):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-
         self.CriarContaBtn.clicked.connect(self.abrir_tela_cadastro)
 
     def abrir_tela_cadastro(self):
-        cadastro = Cadastro()
         cadastro.show()
 
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
     login = Login()
+    cadastro = Cadastro()
     login.show()
-
     sys.exit(qt.exec())
