2f61111bec734f3213cf20db47649ba02fac7dc0
GustavoGuerato <guerato.gustavo14@gmail.com>
2023-12-27 22:36:11 -0300

    creation of the main menu

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from login import Ui_MainWindow as Ui_Login
 from signup import Ui_CadastroWindow
+from mainmenu import Ui_MainWindow as Ui_MainMenu
 
 
 class Cadastro(QMainWindow, Ui_CadastroWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+
+class Inicial(QMainWindow, Ui_MainMenu):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
@@ -15,8 +22,17 @@ class Login(QMainWindow, Ui_Login):
         super().__init__(parent)
         self.setupUi(self)
         self.CriarContaBtn.clicked.connect(self.abrir_tela_cadastro)
+        self.loginBtn.clicked.connect(self.logar)
 
-    def abrir_tela_cadastro(self):
+    def verifica(self):
+        if not self.NomeInput and self.SenhaInput:
+            self.InputErro.setText('preencha todos os dados')
+
+    def logar(self):
+        inicial.show()
+
+    @staticmethod
+    def abrir_tela_cadastro():
         cadastro.show()
 
 
@@ -24,5 +40,6 @@ if __name__ == '__main__':
     qt = QApplication(sys.argv)
     login = Login()
     cadastro = Cadastro()
+    inicial = Inicial()
     login.show()
     sys.exit(qt.exec())
