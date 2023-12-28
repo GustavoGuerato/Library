c7552b605cab93fc5dcec66906c7f7f974ada3e7
GustavoGuerato <guerato.gustavo14@gmail.com>
2023-12-28 01:07:42 -0300

    window logo changed

--- app.py
+++ app.py
@@ -1,50 +1,58 @@
-import password
-import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QIcon
 from login import Ui_MainWindow as Ui_Login
 from signup import Ui_CadastroWindow
 from mainmenu import Ui_MainWindow as Ui_MainMenu
 import mysql.connector
+import sys
 
 conexao = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
-    password="gg08142325",
+    password="nova_senha",
     database="test"
 )
 
 cursor = conexao.cursor()
 
 
-class Cadastro(QMainWindow, Ui_CadastroWindow):
+class JanelaComIcone(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.definir_icone_janela()
+
+    def definir_icone_janela(self):
+
+        icone = QIcon(r'LibraConnect.ico')
+        self.setWindowIcon(icone)
+
+
+class Cadastro(JanelaComIcone, Ui_CadastroWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
 
 
-class Inicial(QMainWindow, Ui_MainMenu):
+class Inicial(JanelaComIcone, Ui_MainMenu):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
 
 
-class Login(QMainWindow, Ui_Login):
+class Login(JanelaComIcone, Ui_Login):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         self.CriarContaBtn.clicked.connect(self.abrir_tela_cadastro)
         self.loginBtn.clicked.connect(self.logar)
 
-    def verifica(self):
-        if not self.NomeInput and self.SenhaInput:
-            self.InputErro.setText('preencha todos os dados')
+    def logar(self):
+        if not self.NomeInput and not self.SenhaInput:
+            self.InputErro.setText('Preencha todos os dados')
+        else:
+            inicial.show()
 
-    @staticmethod
-    def logar():
-        inicial.show()
-
-    @staticmethod
-    def abrir_tela_cadastro():
+    def abrir_tela_cadastro(self):
         cadastro.show()
 
 
